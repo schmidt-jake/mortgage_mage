7fdb2a03ad4ddfe66c057402d207c1f03efe95bd
Jake Schmidt <jake.schmidt@utexas.edu>
2021-09-19 16:57:03 -0500

    feat: add pre-commit and poetry

--- mortgage_mage/rate_scraper.py
+++ mortgage_mage/rate_scraper.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import requests
+import structlog
+
+logger = structlog.get_logger()
 
 
 def get_rate_distribution(
@@ -13,18 +16,21 @@ def get_rate_distribution(
     loan_term: int,
     loan_type: str,
 ) -> pd.Series:
+    params = {
+        "price": price,
+        "loan_amount": loan_amount,
+        "minfico": minfico,
+        "maxfico": maxfico,
+        "state": state,
+        "rate_structure": rate_structure,
+        "loan_term": loan_term,
+        "loan_type": loan_type,
+    }
+    logger.info("GETting rates...", **params)
     response = requests.get(
         url="https://www.consumerfinance.gov/oah-api/rates/rate-checker",
-        params={  # type: ignore
-            "price": price,
-            "loan_amount": loan_amount,
-            "minfico": minfico,
-            "maxfico": maxfico,
-            "state": state,
-            "rate_structure": rate_structure,
-            "loan_term": loan_term,
-            "loan_type": loan_type,
-        },
+        params=params,  # type: ignore
+        timeout=2,
     )
     response.raise_for_status()
     data = pd.Series(response.json()["data"], name="num_lenders")
@@ -52,8 +58,10 @@ def sweep(
                 loan_type="conf",
             )
             for loan_to_value in np.arange(
-                min_loan_to_value, 1.0, loan_to_value_interval
-            )
+                start=min_loan_to_value,
+                stop=1.0,
+                step=loan_to_value_interval,
+            ).round(2)
         }
     )
     data.columns.name = "loan_to_value"
